09db0c2284d7f6ded90ceafc90ef3712e969ea57
cjc2011 <chenJiaCheng2017@gmail.com>
2019-05-08 15:08:59 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
-        >1-1</a>
+        >1-2</a>
       </div>
     </div>
   </section>
