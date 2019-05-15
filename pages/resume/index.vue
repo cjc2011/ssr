518232cf5ff23cd744e0d4562a5d1d75b9137584
cjc2011 <chenJiaCheng2017@gmail.com>
2019-05-15 18:15:35 +0800

    update

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -57,26 +57,55 @@
               <li>
                 <p class="section-content">
                   <i class="iconfont icon-dot"></i>
-                  能够编写语义化HTML, 模块化Css, 熟练使用<strong>Sass</strong>、<strong>Stylus</strong>预处理器及<strong>BEM</strong>规范
+                  能够编写语义化HTML, 模块化Css, 熟练使用
+                  <strong>Sass</strong>、
+                  <strong>Stylus</strong>预处理器及
+                  <strong>BEM</strong>规范
                 </p>
               </li>
               <li>
                 <p class="section-content">
                   <i class="iconfont icon-dot"></i>
-                  熟练掌握原生<strong>JavaScript</strong>, 能够脱离js类库coding, 对<strong>ES6</strong>、<strong>TypeScript</strong>有项目实践经验
+                  熟练掌握原生
+                  <strong>JavaScript</strong>, 能够脱离js类库coding, 对
+                  <strong>ES6</strong>、
+                  <strong>TypeScript</strong>有项目实践经验
                 </p>
               </li>
               <li>
                 <p class="section-content">
                   <i class="iconfont icon-dot"></i>
-                  熟练使用<strong>Vue</strong>、<strong>React</strong>、<strong>Riot</strong>等框架, 阅读过Vue的部分源码，了解<strong>Vuex</strong>、<strong>Vue-Router</strong>代码运行原理，使用过<strong>Nuxt</strong>构建过SSR。
-                  使用<strong>React-Native</strong>、<strong>React-Redux</strong>编写过个人项目，
+                  熟练使用
+                  <strong>Vue</strong>、
+                  <strong>React</strong>、
+                  <strong>React-Native</strong>、
+                  <strong>Riot</strong>等框架以及其相关生态产品
+                  <strong>Vuex</strong>、
+                  <strong>Vue-Router</strong>、
+                  <strong>React-Router</strong>、
+                  <strong>React-Redux</strong>、
+                  <strong>Mobx</strong>。阅读过Vue的部分源码，了解
+                  <strong>Vuex</strong>、
+                  <strong>Vue-Router</strong>代码运行原理，使用过
+                  <strong>Nuxt</strong>构建过SSR。
                 </p>
               </li>
               <li>
                 <p class="section-content">
                   <i class="iconfont icon-dot"></i>
-
+                  熟悉前端构建
+                  <strong>webpack</strong>,
+                  <strong>gulp</strong>及单元测试
+                  <strong>jest</strong>并有实践经验
+                </p>
+              </li>
+              <li>
+                <p class="section-content">
+                  <i class="iconfont icon-dot"></i>
+                  熟悉
+                  <strong>Linux</strong>常用操作，有
+                  <strong>NodeJS</strong>、
+                  <strong>Express</strong>开发及自动化部署经验
                 </p>
               </li>
               <li>
@@ -90,13 +119,52 @@
                   <strong>VScode</strong>作为日常开发工具。
                 </p>
               </li>
-              <li>
-                <p class="section-content">
-                  <i class="iconfont icon-dot"></i>
-                  熟悉<strong>Linux</strong>常用操作，有<strong>NodeJS</strong>、<strong>Express</strong>开发及自动化部署经验
-                </p>
-              </li>
             </ul>
+          </div>
+        </section>
+        <section class="practice">
+          <header class="section-hd">
+            <span class="section-title-l"></span>
+            <h2 class="section-title">工作经历</h2>
+            <span class="section-title-r"></span>
+          </header>
+          <div class="section-bd">
+            <div class="item">
+              <header class="item-hd">
+                <span class="item-time">2018.10 ~ 至今</span>
+                <a class="btn item-more" href="http://www.talk-cloud.com/" target="_blank">Link</a>
+                <h3 class="item-name">北京拓课网络科技有限公司</h3>
+              </header>
+              <div class="item-bd">
+                <p class="section-content">
+                  在此工作期间负责智慧网校和柠檬会议项目的开发任务，在开发过程中对项目细节的具体任务分配，与产品后端同事协调项目的进度，对新入职的同事进行技术分享。
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <header class="item-hd">
+                <span class="item-time">2017.03 ~ 2018.10</span>
+                <a class="btn item-more" href="https://www.jiandanxinli.com/" target="_blank">Link</a>
+                <h3 class="item-name">北京竹间科技有限公司 (简单心理)</h3>
+              </header>
+              <div class="item-bd">
+                <p class="section-content">
+                  在此工作期间负责网站、微信公众号、微信小程序的整体模块更新迭代,新业务线产品开发及市场推广活动技术支持；期间配合产品后端同事开发了：简单心理训练营，自助训练，读书会、智能聊天机器人等多个产品；
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <header class="item-hd">
+                <span class="item-time">2016.10 ~ 2017.02</span>
+                <a class="btn item-more" href="http://sijiyouwan.com/" target="_blank">Link</a>
+                <h3 class="item-name">北京司机游玩网络科技有限公司</h3>
+              </header>
+              <div class="item-bd">
+                <p class="section-content">
+                  使用各种Web技术进行PC端、WAP端官网及APP内H5页面的开发，同时结合后台开发技术模拟整体效果。基于需求，快速选择合适的框架，并提高代码的复用性和可读性；
+                </p>
+              </div>
+            </div>
           </div>
         </section>
       </div>
@@ -111,69 +179,101 @@
             <div class="item">
               <header class="item-hd">
                 <span class="item-time">2018.11</span>
-                <a class="btn item-more" href="https://github.com/cjc2011/school_app" target="_blank">Git</a>
+                <a
+                  class="btn item-more"
+                  href="https://github.com/cjc2011/school_app"
+                  target="_blank"
+                >Git</a>
                 <h3 class="item-name">智慧网校</h3>
               </header>
               <div class="item-bd">
                 <p class="section-content">
                   管理后台及网校移动端框架和工具采用
-                  <strong>Vue</strong>
-                  <strong>ElementUI</strong>，项目剥离了请求接口、页面等层级，对公用组件及方法进行了封装。
-                  使用<strong>Vuex</strong>进行全局的数据状态管理降低了功能模块之间的耦合度及数据交互，使用动态路由匹配对用户进行权限管理，使用<strong>Echarts</strong>可视化交易流水，课时安排等数据统计；
+                  <em>Vue</em>
+                  <em>ElementUI</em>，项目剥离了请求接口、页面等层级，对公用组件及方法进行了封装。
+                  使用
+                  <em>Vuex</em>进行全局的数据状态管理降低了功能模块之间的耦合度及数据交互，使用动态路由匹配对用户进行权限管理，使用
+                  <em>Echarts</em>可视化交易流水，课时安排等数据统计；
                 </p>
               </div>
             </div>
             <div class="item">
               <header class="item-hd">
-                <span class="item-time">2015.12</span>
-                <a class="btn item-more" href="http://lanshan.studio" target="_blank">Link</a>
-                <h3 class="item-name">蓝山工作室主页</h3>
+                <span class="item-time">2018.8</span>
+                <a class="btn item-more" href="https://www.jiandanxinli.com/experts/" target="_blank">Link</a>
+                <h3 class="item-name">咨询师模块重构</h3>
               </header>
               <div class="item-bd">
                 <p class="section-content">
-                  <strong>界面设计</strong>
-                  <strong>前端开发</strong>
-                  利用
-                  <em>HTML5</em>、
-                  <em>CSS3</em>、
-                  <em>Sass</em>、
-                  <em>Compass</em>、
-                  <em>雪碧图</em>等各种技术来丰富展示
-                  <em>响应式</em>单页，以
-                  <em>Handlebars</em>作为模板引擎处理数据逻辑，基于
-                  <em>jQuery</em>进行了一些组件开发；搭建并维护了线上
-                  <em>Ubuntu</em>系统
-                  <em>nginx</em>服务器环境。
+                  该项目基于<em>Vue</em>、<em>Nuxt</em>重构
                 </p>
               </div>
             </div>
             <div class="item">
               <header class="item-hd">
-                <span class="item-time">2015.10</span>
-                <a class="btn item-more" href="http://cqupt-cs.coding.congm.in" target="_blank">Demo</a>
-                <h3 class="item-name">新版计算机科学与技术学院主页</h3>
+                <span class="item-time">2018.5</span>
+                <a class="btn item-more qr-code" href="http://cqupt-cs.coding.congm.in" target="_blank">QRcode</a>
+                <div class="qr-code-img">
+                  <div class="pop-arrow"></div>
+                  <img src="../../assets/qr-code.png" alt="二维码">
+                </div>
+                <h3 class="item-name">自助心理训练小程序</h3>
               </header>
               <div class="item-bd">
                 <p class="section-content">
-                  <strong>界面设计</strong>
-                  <strong>页面重构</strong>
-                  基于UI框架
-                  <em>Bootstrap</em>深度定制了一套样式，实现多终端
-                  <em>响应式布局</em>与良好的兼容性(IE8)，针对用户调研意见进行了一系列改进优化，极大提升用户体验。
+                  <em>wepy</em>公用组件及方法进行了封装。 使用 Vuex进行全局的数据状态管理降低了功能模块之间的耦合度及数据交互进行全局的数据状态管理降低了功能模块之间的耦合度及数据交互，使用动态路由匹配对用户进行权限管理，使用
                 </p>
               </div>
             </div>
             <div class="item">
               <header class="item-hd">
-                <span class="item-time">2015.06</span>
-                <!--<a class="btn item-more" href="javascript:void(0);" target="_blank">Link</a>-->
-                <h3 class="item-name">cBlog班级博客系统</h3>
+                <span class="item-time">2018.03</span>
+                <a class="btn item-more qr-code" href="http://cqupt-cs.coding.congm.in" target="_blank">QRcode</a>
+                <div class="qr-code-img">
+                  <div class="pop-arrow"></div>
+                  <img src="../../assets/qr-code.png" alt="二维码">
+                </div>
+                <h3 class="item-name">心理预约咨询小程序</h3>
               </header>
               <div class="item-bd">
                 <p class="section-content">
-                  <strong>产品设计</strong>
-                  <strong>界面设计</strong>
-                  <strong>前端开发</strong> 该系统经历了一系列软件工程流程，从需求分析到设计系统功能界面，然后开发联调测试，实现前后端分离的数据交互，上线后不断的维护与重构代码，优化代码性能与规范。
+                  小程序骨架屏 wepy 预渲染 小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染小程序骨架屏 wepy 预渲染 小程序骨架屏 wepy 预渲染小程序骨架屏
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <header class="item-hd">
+                <span class="item-time">2017.11</span>
+                <a class="btn item-more qr-code" href="https://www.jiandanxinli.com/learns" target="_blank">Link</a>
+                <h3 class="item-name">简单心理训练营/读书会(移动端)</h3>
+              </header>
+              <div class="item-bd">
+                <p class="section-content">
+                  简单心理训练营简单心理训练营简单心理训练营简单心理训练营简单心理训练营简单心理训练营简单心理训练营简单心理训练营
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <header class="item-hd">
+                <span class="item-time">2017.6</span>
+                <a class="btn item-more qr-code" href="https://www.jiandanxinli.com/testings" target="_blank">Link</a>
+                <h3 class="item-name">心理测试模块(移动端) </h3>
+              </header>
+              <div class="item-bd">
+                <p class="section-content">
+                  <em>riot</em> <em>turbolink</em>心理测试模块心理测试模块心理测试模块心理测试模块心理测试模块心理测试模块心理测试模块心理测试模块心理测试模块心理测试模块心理测试模块
+                </p>
+              </div>
+            </div>
+            <div class="item">
+              <header class="item-hd">
+                <span class="item-time">2016.7</span>
+                <a class="btn item-more qr-code" href="https://www.jiandanxinli.com/testings" target="_blank">Link</a>
+                <h3 class="item-name">四季游玩移动端(移动端) </h3>
+              </header>
+              <div class="item-bd">
+                <p class="section-content">
+                  四季游玩移动端 四季游玩移动端四季游玩移动端四季游玩移动端四季游玩移动端四季游玩移动端四季游玩移动端四季游玩移动端<em>四季游玩移动端</em>四季游玩移动端四季游玩移动端
                 </p>
               </div>
             </div>
