051b881ae0e1ba5fdcb3b4d5e87a994457074c05
cjc2011 <chenJiaCheng2017@gmail.com>
2019-05-24 15:59:44 +0800

    axios conf

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = process.env.NODE_ENV == 'production' ? 'http://www.chenjiacheng.cn' : 'http://localhost:3001';
+const url = process.env.NODE_ENV == 'production' ? 'http://resume.chenjiacheng.cn' : 'http://localhost:3001';
 
 export default axios.create({
   baseURL: url
